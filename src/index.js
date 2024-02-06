c4f5cc871de34f105b659158a4d3f71e3fc127a2
MiguelAChavez <129125500+MiguelAChavez@users.noreply.github.com>
2024-02-06 16:59:19 -0300

    setup Prisma with MySQL database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import app from "./app.js";
+import app from "./app/app.js";
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
