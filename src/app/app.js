8b9b09fea269e868ae80b7fe0dd09cf0bb7ea1a6
MiguelAChavez <129125500+MiguelAChavez@users.noreply.github.com>
2024-02-17 08:32:29 -0300

    Update app.js

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,17 @@
 import express from "express";
+import cors from "cors";
+
+const originCallback = (origin, callback) => {
+  const allowedOrigins = ["http://localhost:5173"];
+  const isAllowed = allowedOrigins.includes(origin);
+  isAllowed ? callback(null, true) : callback(new Error("Not allowed by CORS"));
+};
 const app = express();
 app.disable("x-powered-by");
 app.use(express.json());
+app.use(
+  cors({
+    origin: originCallback,
+  })
+);
 export default app;
